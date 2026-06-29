6c855061d346ff5a9c957368fd5ee875454b4121
Elena Eyre <23263129+elenu@users.noreply.github.com>
2026-06-29 17:26:35 +0200

    Change omnipath cache directory option
    Updated the omnipath cache directory option to a specific path.

--- R/download_omnipath.R
+++ R/download_omnipath.R
@@ -2,7 +2,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocMana
 if (!requireNamespace("OmnipathR", quietly = TRUE)) install.packages("OmnipathR")
 
 library(OmnipathR)
-options(omnipath.cache = ".")
+options(omnipath.cache = "./Your_directory/")
 
 acad_interactions <- import_all_interactions(license = "academic", organism = 9606)
 write_csv(acad_interactions, file = "./data/omnipath_interactions_table.csv")
